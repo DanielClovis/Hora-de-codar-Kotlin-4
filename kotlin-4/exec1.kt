4fa1497d9ac799b3875cd91fa83eead0c9ee7251
Daniel Clovis da Costa <danielclovis233@gmail.com>
2026-03-22 17:31:17 -0300

    Add files via upload

---
+++ kotlin-4/exec1.kt
@@ -0,0 +1,26 @@
+package HoraDeCodar04
+
+
+val estudantes: MutableList<String> = mutableListOf()
+
+private fun main() {
+    return cadastrarEstudante()
+}
+
+fun cadastrarEstudante() {
+    println("Digite \"PARE\" ")
+    println("Qual o nome do estudante?")
+    val nomeEstudante = readln().trim()
+    if (nomeEstudante.uppercase() == "PARE") return imprimirInformacoes()
+    else if (nomeEstudante.isNotBlank()) {
+        estudantes.add(nomeEstudante)
+        return main()
+    }
+    println("Nome inválido!")
+    return cadastrarEstudante()
+}
+
+fun imprimirInformacoes() {
+    println("Quantidade de estudantes: ${estudantes.size}")
+    println("Nomes: $estudantes")
+}
